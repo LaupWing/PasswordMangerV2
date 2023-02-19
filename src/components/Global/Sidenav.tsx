84dfcbd76ddea16dba61911fce34b61df92f6f40
LaupWing <loc_strict@hotmail.com>
2023-02-19 19:10:34 +0100

    update

--- src/components/Global/Sidenav.tsx
+++ src/components/Global/Sidenav.tsx
@@ -1,6 +1,9 @@
+import Link from "next/link"
+import { IconItems } from "~/components/Elements"
+
 const Sidenav = () => {
 
-   const linkStyle = "w-48 p-2 py-1 rounded-md flex items-center"
+   const linkStyles = "w-48 p-2 py-1 rounded-md flex items-center"
 
    return (
       <div className="bg-main-secondary p-4 border-r-2 border-black">
@@ -10,7 +13,13 @@ const Sidenav = () => {
             className="w-20 mx-auto mb-6"
          />
          <ul className="text-white text-sm flex flex-col">
-            
+            <Link
+               className={linkStyles}
+               href={"/"}
+            >
+               <IconItems className="w-5 mr-1"/>
+               Alle Items
+            </Link>
          </ul>
       </div>
    )
