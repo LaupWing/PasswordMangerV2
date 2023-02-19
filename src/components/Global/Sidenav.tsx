ff1e2fc95195760715a67899199021432c148b40
LaupWing <loc_strict@hotmail.com>
2023-02-19 19:05:47 +0100

    sidenav

--- src/components/Global/Sidenav.tsx
+++ src/components/Global/Sidenav.tsx
@@ -1,11 +1,17 @@
 const Sidenav = () => {
+
+   const linkStyle = "w-48 p-2 py-1 rounded-md flex items-center"
+
    return (
-      <div className="bg-main-secondary p-4 border-r border-black">
+      <div className="bg-main-secondary p-4 border-r-2 border-black">
          <img 
             src="/assets/logo.png" 
             alt="logo" 
             className="w-20 mx-auto mb-6"
-         />      
+         />
+         <ul className="text-white text-sm flex flex-col">
+            
+         </ul>
       </div>
    )
 }
