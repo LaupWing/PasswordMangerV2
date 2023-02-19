929f070e839120743aa9587f74556a1ab65f1e58
LaupWing <loc_strict@hotmail.com>
2023-02-19 19:22:36 +0100

    Sidenav

--- src/components/Global/Sidenav.tsx
+++ src/components/Global/Sidenav.tsx
@@ -1,10 +1,10 @@
+import clsx from "clsx"
 import Link from "next/link"
+import { useRouter } from "next/router"
+import { FC, PropsWithChildren } from "react"
 import { IconItems } from "~/components/Elements"
 
 const Sidenav = () => {
-
-   const linkStyles = "w-48 p-2 py-1 rounded-md flex items-center"
-
    return (
       <div className="bg-main-secondary p-4 border-r-2 border-black">
          <img 
@@ -13,15 +13,34 @@ const Sidenav = () => {
             className="w-20 mx-auto mb-6"
          />
          <ul className="text-white text-sm flex flex-col">
-            <Link
-               className={linkStyles}
-               href={"/"}
-            >
+            <SidenavLink href="/">
                <IconItems className="w-5 mr-1"/>
                Alle Items
-            </Link>
+            </SidenavLink>
          </ul>
       </div>
    )
 }
 export default Sidenav
+
+interface SidenavLinkProps extends PropsWithChildren {
+   href: string
+}
+
+const SidenavLink:FC<SidenavLinkProps> = ({ children, href }) => {
+   const router = useRouter()
+   const baseStyles = "w-48 p-2 py-1 rounded-md flex items-center"
+   const styles = clsx(
+      baseStyles,
+      router.pathname === href && "bg-blue-600"
+   )
+
+   return (
+      <Link
+         className={styles}
+         href={href}
+      >
+         { children }
+      </Link>
+   )
+}
