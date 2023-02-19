ed66719ad0d702320922dada7c35f1b7403e1f89
LaupWing <loc_strict@hotmail.com>
2023-02-19 18:51:10 +0100

    favorite

--- src/components/Elements/Icons.tsx
+++ src/components/Elements/Icons.tsx
@@ -1,6 +1,7 @@
 import type { IconType } from "react-icons"
 import { FaEye, FaEyeSlash } from "react-icons/fa"
 import { BsGridFill } from "react-icons/bs"
+import { AiFillStar } from "react-icons/ai"
 
 export const IconEye:IconType = (props) => {
    return (
@@ -17,5 +18,11 @@ export const IconEyeSlash:IconType = (props) => {
 export const IconItems:IconType = (props) => {
    return (
       <BsGridFill {...props}/>
+   )
+} 
+
+export const IconFavorite:IconType = (props) => {
+   return (
+      <AiFillStar {...props}/>
    )
 } 
