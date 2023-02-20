3b06f9b4aa6490e2f466e0dbd4efc99f711821da
LaupWing <loc_strict@hotmail.com>
2023-02-20 14:00:42 +0100

    update

--- src/slices/authSlice.ts
+++ src/slices/authSlice.ts
@@ -1,5 +1,5 @@
 import { createSlice, Dispatch, PayloadAction } from "@reduxjs/toolkit"
-import { browserSessionPersistence, setPersistence, signInWithEmailAndPassword } from "firebase/auth"
+import { browserSessionPersistence, setPersistence, signInWithEmailAndPassword, UserCredential } from "firebase/auth"
 import { auth } from "~/firebase"
 import checkSecretKey from "~/lib/checkSecretKey"
 import { store } from "~/redux/store"
@@ -11,8 +11,9 @@ export const counterSlice = createSlice({
       expire_time: 0
    },
    reducers: {
-      setExperTime: (state, action: PayloadAction<number>) => {
-         state.expire_time = action.payload
+      setExperTime: (state, action: PayloadAction<UserCredential>) => {
+         const lastSignInTime = action.payload.user.metadata.lastSignInTime
+         state.expire_time = Number(lastSignInTime) + ((60 * 30)*1000)  
       },
       decrement: (state) => {
          state.value -= 1
@@ -24,12 +25,12 @@ export const { setExperTime, decrement } = counterSlice.actions
 
 export const login = 
    (email: string, password: string, secretKey: string) => 
-   async (dispatch: Dispatch, getState: typeof store.getState) => {
+   async (dispatch: Dispatch) => {
       try{
          await setPersistence(auth, browserSessionPersistence)
          const user = await signInWithEmailAndPassword(auth, email, password)
          await checkSecretKey(secretKey, user.user.uid)
-         
+         await dispatch(setExperTime(user))
       }catch(e){
          auth.signOut()
          throw e
