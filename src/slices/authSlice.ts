81387375ae794b9e3541b9d69617543b71fdf212
LaupWing <loc_strict@hotmail.com>
2023-02-20 13:58:58 +0100

    expiter time

--- src/slices/authSlice.ts
+++ src/slices/authSlice.ts
@@ -1,37 +1,37 @@
-import { createSlice, Dispatch } from "@reduxjs/toolkit"
+import { createSlice, Dispatch, PayloadAction } from "@reduxjs/toolkit"
 import { browserSessionPersistence, setPersistence, signInWithEmailAndPassword } from "firebase/auth"
 import { auth } from "~/firebase"
+import checkSecretKey from "~/lib/checkSecretKey"
 import { store } from "~/redux/store"
 
 export const counterSlice = createSlice({
    name: "counter",
    initialState: {
       value: 0,
+      expire_time: 0
    },
    reducers: {
-      increment: (state) => {
-         state.value += 1
+      setExperTime: (state, action: PayloadAction<number>) => {
+         state.expire_time = action.payload
       },
       decrement: (state) => {
          state.value -= 1
       },
-      incrementByAmount: (state, action) => {
-         state.value += action.payload
-      },
    },
 })
 
-export const { increment, decrement, incrementByAmount } = counterSlice.actions
+export const { setExperTime, decrement } = counterSlice.actions
 
 export const login = 
-   (email: string, password: string) => 
+   (email: string, password: string, secretKey: string) => 
    async (dispatch: Dispatch, getState: typeof store.getState) => {
       try{
          await setPersistence(auth, browserSessionPersistence)
          const user = await signInWithEmailAndPassword(auth, email, password)
-
+         await checkSecretKey(secretKey, user.user.uid)
          
       }catch(e){
+         auth.signOut()
          throw e
       }
    }
