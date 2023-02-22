5bd1240fdac5514c22310f4fa3bf659c0a6dc48c
LaupWing <loc_strict@hotmail.com>
2023-02-22 19:22:59 +0100

    update

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,7 +1,13 @@
 const LoginPage = () => {
    return (
-      <div className="flex items-center">
-         
+      <div className="flex items-center flex-1">
+         <form className="w-64 mx-auto flex flex-col items-center text-white text-sm">
+            <img 
+               src="/assets/logo.png" 
+               alt="logo" 
+               className="w-28 mb-8"
+            />
+         </form>
       </div>
    )
 }
