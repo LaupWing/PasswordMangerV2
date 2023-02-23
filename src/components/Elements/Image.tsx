80ed8a72c1a62b5baa1643eeea4f36f0b4b161de
LaupWing <loc_strict@hotmail.com>
2023-02-23 19:17:24 +0100

    update

--- src/components/Elements/Image.tsx
+++ src/components/Elements/Image.tsx
@@ -1,3 +1,11 @@
-export const Image = () => {
-   return <div>Image</div>
+import { FC } from "react"
+
+export const Image:FC = () => {
+   return (
+      <img 
+         className="rounded-md mr-2 bg-white object-contain"
+         src="" 
+         alt="" 
+      />
+   )
 }
