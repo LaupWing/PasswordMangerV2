89df3c40293bd9888154ba7894098ee17977559c
LaupWing <loc_strict@hotmail.com>
2023-02-23 18:14:13 +0100

    Websites

---
+++ src/components/Sections/WebsitesList.tsx
@@ -0,0 +1,6 @@
+const WebsitesList = () => {
+   return (
+      <div className="h-full md:border-r-2 w-full md:w-auto border-black p-3 pt-6 flex flex-col">WebsitesList</div>
+   )
+}
+export default WebsitesList
