2e5c3910ed9bc7458e6511bcceab13a4dded448f
LaupWing <loc_strict@hotmail.com>
2023-02-23 18:15:20 +0100

    export website lists

--- src/components/Sections/WebsitesList/WebsitesList.tsx
+++ src/components/Sections/WebsitesList/WebsitesList.tsx
@@ -1,6 +1,5 @@
-const WebsitesList = () => {
+export const WebsitesList = () => {
    return (
       <div className="h-full md:border-r-2 w-full md:w-auto border-black p-3 pt-6 flex flex-col">WebsitesList</div>
    )
 }
-export default WebsitesList
---
+++ src/components/Sections/index.ts
@@ -0,0 +1,1 @@
+export { WebsitesList } from "./WebsitesList/WebsitesList"
