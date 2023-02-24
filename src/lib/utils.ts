78abcf03201775a476eb837b7f12629b4b44fdda
LaupWing <loc_strict@hotmail.com>
2023-02-24 12:51:55 +0100

    missing class

---
+++ src/lib/utils.ts
@@ -0,0 +1,7 @@
+export const missingClass = (string?: string, prefix?: string) => {
+   if(!string){
+      return true
+   }
+   const regex = new RegExp(` ?${prefix}`, "g")
+   return string.match(regex) === null
+}
