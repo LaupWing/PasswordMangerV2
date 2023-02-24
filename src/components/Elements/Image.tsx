f21c4243805acde96e6720c88f4e7dd71d166c26
LaupWing <loc_strict@hotmail.com>
2023-02-24 12:55:34 +0100

    update

--- src/components/Elements/Image.tsx
+++ src/components/Elements/Image.tsx
@@ -1,10 +1,17 @@
+import clsx from "clsx"
 import { FC } from "react"
+import { missingClass } from "~/lib/utils"
 
 export const Image:FC<HTMLImageElement> = ({
    className = "",
    src
 }) => {
    const logo = `https://logo.clearbit.com/${src}`
+
+   const styles = clsx(
+      missingClass(className, "w-") && "w-10",
+      missingClass(className, "h-") && "h-10",
+   )
 
    return (
       <img 
