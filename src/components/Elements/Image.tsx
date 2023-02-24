2e075c67dd9bdba60b56b28e0575814682cee709
LaupWing <loc_strict@hotmail.com>
2023-02-24 12:47:25 +0100

    Update

--- src/components/Elements/Image.tsx
+++ src/components/Elements/Image.tsx
@@ -1,11 +1,16 @@
 import { FC } from "react"
 
-export const Image:FC = () => {
+export const Image:FC<HTMLImageElement> = ({
+   className = "",
+   src
+}) => {
+   const logo = `https://logo.clearbit.com/${src}`
+
    return (
       <img 
-         className="rounded-md mr-2 bg-white object-contain"
-         src="" 
-         alt="" 
+         className={"rounded-md mr-2 bg-white object-contain " + className}
+         src={logo} 
+         alt="Logo of website" 
       />
    )
 }
