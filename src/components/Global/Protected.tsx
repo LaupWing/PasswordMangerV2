c59100934421505ae18ce3f2562a8d206d4afdee
LaupWing <loc_strict@hotmail.com>
2023-03-04 16:51:35 +0100

    Prototected

---
+++ src/components/Global/Protected.tsx
@@ -0,0 +1,13 @@
+import { FC, PropsWithChildren } from "react"
+import { useAppSelector } from "~/redux/hooks"
+
+export const Protected:FC<PropsWithChildren> = ({children}) => {
+   const {time_left} = useAppSelector(state => state.auth)
+
+   console.log(time_left)
+   return (
+      <>
+         {children}
+      </>
+   )
+}
