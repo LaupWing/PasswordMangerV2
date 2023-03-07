8bbc2d4790123a189128c9cc249009613cc5e23f
LaupWing <loc_strict@hotmail.com>
2023-03-07 11:50:39 +0100

    action_btn_styles

--- src/components/Sections/Detail/DetailActions.tsx
+++ src/components/Sections/Detail/DetailActions.tsx
@@ -1,3 +1,14 @@
+import { IconEdit } from "~/components/Elements"
+
 export const DetailActions = () => {
-   return <div>DetailActions</div>
+   const action_btn_styles = "py-2 focus:outline-none focus:ring-2 focus:border-blue-600 px-4 bg-main-secondary rounded mr-4 flex items-center"
+
+   return (
+      <div className="text-white text-sm flex justify-end">
+         <button className={action_btn_styles}>
+            <IconEdit className="w-4 mr-2"/>
+            Bewerk
+         </button>
+      </div>
+   )
 }
