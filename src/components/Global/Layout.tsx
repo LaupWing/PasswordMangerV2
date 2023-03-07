5553fdd8031201a79f0a0ae334de90e123f0bc71
LaupWing <loc_strict@hotmail.com>
2023-03-07 11:25:21 +0100

    Layout

--- src/components/Global/Layout.tsx
+++ src/components/Global/Layout.tsx
@@ -1,9 +1,16 @@
+import { FC, PropsWithChildren } from "react"
 import { Protected } from "./Protected"
+import Sidenav from "./Sidenav"
+import Topnav from "./Topnav"
 
-export const Layout = () => {
+export const Layout:FC<PropsWithChildren> = ({children}) => {
    return (
       <Protected>
-         
+         <Sidenav />
+         <div className="flex flex-col flex-1 w-screen lg:w-auto relative">
+            <Topnav/>
+            {children}
+         </div>
       </Protected>
    )
 }
