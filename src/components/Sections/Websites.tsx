e711d613c4c6988e8b462f618df3a9c617aec213
LaupWing <loc_strict@hotmail.com>
2023-03-07 11:07:26 +0100

    update

--- src/components/Sections/Websites.tsx
+++ src/components/Sections/Websites.tsx
@@ -1,14 +1,15 @@
 import { FC } from "react"
 import { PasswordType } from "types"
 import { useAppSelector } from "~/redux/hooks"
-import { ImageContainer } from "~/components/Elements"
+import { IconSearch, ImageContainer } from "~/components/Elements"
 
 export const Websites = () => {
    const { passwords } = useAppSelector(state => state.passwords)
    
    return (
-      <div className="h-full md:border-r-2 w-full md:w-auto border-black p-3 pt-6 flex flex-col">
-         <ul className="text-white text-sm md:w-80 w-full overflow-y-auto">
+      <div className="h-full md:border-r-2 w-full md:w-80 border-black p-3 pt-6 flex flex-col">
+         <AddWebsiteForm />
+         <ul className="text-white text-sm w-full overflow-y-auto">
             {passwords.map(password => (
                <WebsiteItem
                   password={password}
@@ -30,5 +31,24 @@ const WebsiteItem:FC<{
             <p className="text-xs text-gray-400">{password.username}</p>
          </div>
       </li>
+   )
+}
+
+const AddWebsiteForm:FC = () => {
+   return (
+      <div className="mb-4 flex text-accent-grey">
+         <div className="bg-main-tertiare rounded flex p-2 flex-1 items-center">
+            <IconSearch size={22} />
+            <input 
+               type="text" 
+               className="bg-main-tertiare text-sm flex-1 outline-none ml-2"
+            />
+         </div>
+         <button
+            className="w-12 text-2xl text-white rounded ml-4 bg-blue-600"
+         >
+            +
+         </button>
+      </div>
    )
 }
