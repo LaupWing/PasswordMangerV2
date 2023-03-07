4b4bbef098ce7b488b29dd8bc0f0bc8a1bc57030
LaupWing <loc_strict@hotmail.com>
2023-03-07 12:09:57 +0100

    update

--- src/components/Sections/Detail/DetailContent.tsx
+++ src/components/Sections/Detail/DetailContent.tsx
@@ -1,6 +1,26 @@
+import { FC } from "react"
+
 export const DetailContent = () => {
    return <div>DetailContent</div>
 }
 
 const Directories = () => {}
 const Info = () => {}
+
+interface InfoFieldProps {
+   is_password?: boolean
+   show_password?: boolean
+   value: string
+   label: string
+}
+const InfoField:FC<InfoFieldProps> = () => {
+   return (
+      <div 
+         className="py-4 px-3 rounded-md hover:bg-main-secondary text-sm flex items-center 
+         justify-between overflow-hidden select-none"
+         // onMouseOver={() => }
+      >
+
+      </div>
+   )
+}
