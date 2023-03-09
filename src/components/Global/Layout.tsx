e45c09af10b51f4769751c2be8f82cd7f6912213
LaupWing <loc_strict@hotmail.com>
2023-03-09 19:52:11 +0100

    yeah

--- src/components/Global/Layout.tsx
+++ src/components/Global/Layout.tsx
@@ -1,11 +1,27 @@
-import { FC, PropsWithChildren } from "react"
+import type { FC, PropsWithChildren } from "react"
+import { useEffect } from "react"
+import toast, { Toaster } from "react-hot-toast"
 import { Protected } from "./Protected"
 import Sidenav from "./Sidenav"
 import Topnav from "./Topnav"
 
 export const Layout:FC<PropsWithChildren> = ({children}) => {
+   useEffect(() => {
+      setTimeout(() =>{
+         toast(
+            <div className="bg-red-500">
+
+            </div>
+         )
+      }, 4000)
+   }, [])
    return (
       <Protected>
+         <Toaster
+            toastOptions={{
+               className: "bg-red-500",
+            }}
+         />
          <Sidenav />
          <div className="flex flex-col flex-1 w-screen lg:w-auto relative">
             <Topnav/>
