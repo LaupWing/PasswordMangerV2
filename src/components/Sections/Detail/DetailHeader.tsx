cfd13fbfce7d48dfba5b2a2cd1ec5968b6ef73e3
LaupWing <loc_strict@hotmail.com>
2023-03-10 15:11:49 +0100

    Detail header

--- src/components/Sections/Detail/DetailHeader.tsx
+++ src/components/Sections/Detail/DetailHeader.tsx
@@ -1,3 +1,24 @@
-export const DetailHeader = () => {
-   return <div>DetailHeader</div>
+import { FC } from "react"
+import { AccountType } from "types"
+import { ImageContainer } from "~/components/Elements"
+
+export const DetailHeader:FC<{account: AccountType}> = ({
+   account
+}) => {
+   return (
+      <header className="py-6 text-white flex">
+         <ImageContainer
+            src={account.url}
+         />
+         <div className="ml-4">
+            <h2 className="text-2xl font-semibold tracking-wide">
+               {account.name}
+               <span>
+
+               </span>
+            </h2>
+            <p className="text-accent-grey">Login</p>
+         </div>
+      </header>
+   )
 }
