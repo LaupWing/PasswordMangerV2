1df28018359ed5bb44d758d709b77905c703b8a3
LaupWing <loc_strict@hotmail.com>
2023-03-10 15:21:23 +0100

    update

--- src/components/Sections/Detail/Detail.tsx
+++ src/components/Sections/Detail/Detail.tsx
@@ -1,3 +1,19 @@
-export const Detail = () => {
-   return <div>Detail</div>
+import { FC } from "react"
+import { AccountType } from "types"
+import { DetailActions } from "./DetailActions"
+import { DetailContent } from "./DetailContent"
+import { DetailHeader } from "./DetailHeader"
+
+export const Detail:FC<{account: AccountType}> = ({
+   account
+}) => {
+   return (
+      <div className="flex-1 items-start p-4 pt-6 max-w-5xl flex flex-col">
+         <div className="flex flex-col w-full md:max-w-lg max-w-xl min-h-0 mx-auto">
+            <DetailActions />
+            <DetailHeader account={account} />
+            <DetailContent account={account} />
+         </div>
+      </div>
+   )
 }
