d37e79c6812915eb752621b5e73f4383741ff01a
LaupWing <loc_strict@hotmail.com>
2023-03-10 19:39:50 +0100

    backdrop

--- src/components/Global/Backdrop.tsx
+++ src/components/Global/Backdrop.tsx
@@ -1,6 +1,12 @@
-const Backdrop = () => {
+import { FC, PropsWithChildren } from "react"
+
+const Backdrop:FC<PropsWithChildren> = ({
+   children
+}) => {
    return (
-      <div className="flex fixed inset-0 z-50">Backdrop</div>
+      <div className="flex fixed inset-0 z-50 bg-black/70">
+         { children }
+      </div>
    )
 }
 export default Backdrop
