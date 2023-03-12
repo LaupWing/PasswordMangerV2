db41643b97a050cb71d3ea246f595bd919bdfb2f
LaupWing <loc_strict@hotmail.com>
2023-03-12 10:34:45 +0100

    udpate

--- src/components/Modals/AccountModal/AccountModal.tsx
+++ src/components/Modals/AccountModal/AccountModal.tsx
@@ -3,8 +3,13 @@ import { Backdrop } from "~/components/Global"
 
 const AccountModal = () => {
    return (
-      <Backdrop>
-
+      <Backdrop className="p-2 items-start flex-col">
+         <form 
+            autoComplete="off"
+            className="w-full max-w-xl border-2 border-black bg-black-default mx-auto rounded overflow-hidden flex flex-col relative min-h-0 mt-[10vh]" 
+         >
+            
+         </form>
       </Backdrop>
    )
 }
