37106fa45d98276ee1b92f353f68c0bc5d37549b
LaupWing <loc_strict@hotmail.com>
2023-03-13 11:23:51 +0100

    udpate

--- src/components/Elements/PasswordMeter.tsx
+++ src/components/Elements/PasswordMeter.tsx
@@ -1,4 +1,5 @@
 import { FC } from "react"
+import { PasswordMeter as _PasswordMeter } from "password-meter"
 
 interface PasswordMeterProps {
    password: string
@@ -14,7 +15,34 @@ export const PasswordMeter:FC<PasswordMeterProps> = ({
    )
 }
 
-const Bar = () => {
+interface BarProps {
+   index: number
+   password: string
+}
+const Bar:FC<BarProps> = ({
+   password,
+   index
+}) => {
+   const strength = () => {
+      const {percent} = new _PasswordMeter().getResult(password) 
+      if(index  === 1 && (percent >= 5 && percent <= 20)){
+         return "bg-red-600"
+      }
+      if(index  <= 2 && (percent > 20 && percent <= 40)){
+         return "bg-red-400"
+      }
+      if(index  <= 3 && (percent > 40 && percent <= 60)){
+         return "bg-yellow-600"
+      }
+      if(index  <= 4 && (percent > 60 && percent <= 80)){
+         return "bg-green-300"
+      }
+      if(index  <= 5 && (percent > 80 && percent <= 100)){
+         return "bg-green-400"
+      }
+      return 'bg-black-lightest'
+   }
+
    return (
       <div className="h-1 w-10 mr-2 duration-200">
 
