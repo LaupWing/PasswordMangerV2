d6029efb1ba2fb2e0de794d0ae79df92b32c5e90
LaupWing <loc_strict@hotmail.com>
2023-03-13 11:37:02 +0100

    udpate

--- src/components/Elements/PasswordOptions.tsx
+++ src/components/Elements/PasswordOptions.tsx
@@ -1,3 +1,17 @@
+import { useState } from "react"
+
 export const PasswordOptions = () => {
-   return <div>PasswordOptions</div>
+   const [show, setShow] = useState(false)
+
+   return (
+      <div className="flex flex-col w-56 duration-500 border-2 overflow-hidden border-main-tertiare rounded-md flex-shrink-0">
+         <button 
+            className="bg-main-tertiare text-white text-xs font-bold tracking-widest uppercase p-1 px-5 flex items-center cursor-pointer select-none"
+            onClick={() => setShow(_show => !_show)}
+         >
+            wachtwoord opties
+            {/* <Icon */}
+         </button>
+      </div>
+   )
 }
