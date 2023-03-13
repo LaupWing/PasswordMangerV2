c5fbc935444e0bc4d7124bad2d971edd0b45a362
LaupWing <loc_strict@hotmail.com>
2023-03-13 11:02:26 +0100

    udpate

--- src/components/Modals/AccountModal/AccountModalInfo.tsx
+++ src/components/Modals/AccountModal/AccountModalInfo.tsx
@@ -1,11 +1,20 @@
+import { FC } from "react"
+import { AccountType } from "types"
 import { Input } from "~/components/Elements"
 
-export const MainInfo = () => {
+export const MainInfo:FC<{edit_account: AccountType}> = ({
+   edit_account
+}) => {
    return (
       <div className="p-4 text-sm border-b-2 border-t-2 py-6 border-black flex flex-col overflow-y-auto">
          <div className="flex items-center sm:w-72 w-60 mb-6">
             <p className="mr-0.5 text-main-tertiare font-bold tracking-wider">www.</p>
-            <Input />
+            <Input 
+               placeholder="URL"
+               autoComplete="off"
+               className="flex-1"
+               // onChange={e =>}
+            />
          </div>
       </div>
    )
