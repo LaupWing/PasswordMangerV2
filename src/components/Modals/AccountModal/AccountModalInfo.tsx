40573cde4fd148e81a728b5e75de28c368b0e802
LaupWing <loc_strict@hotmail.com>
2023-03-13 11:04:19 +0100

    update

--- src/components/Modals/AccountModal/AccountModalInfo.tsx
+++ src/components/Modals/AccountModal/AccountModalInfo.tsx
@@ -2,8 +2,14 @@ import { FC } from "react"
 import { AccountType } from "types"
 import { Input } from "~/components/Elements"
 
-export const MainInfo:FC<{edit_account: AccountType}> = ({
-   edit_account
+interface MainInfoProps {
+   edit_account: AccountType
+   setEditAccount: (account: AccountType) => void
+}
+
+export const MainInfo:FC<MainInfoProps> = ({
+   edit_account,
+   setEditAccount
 }) => {
    return (
       <div className="p-4 text-sm border-b-2 border-t-2 py-6 border-black flex flex-col overflow-y-auto">
@@ -13,7 +19,10 @@ export const MainInfo:FC<{edit_account: AccountType}> = ({
                placeholder="URL"
                autoComplete="off"
                className="flex-1"
-               // onChange={e =>}
+               onChange={e => setEditAccount({
+                  ...edit_account,
+                  url: e.target.value
+               })}
             />
          </div>
       </div>
