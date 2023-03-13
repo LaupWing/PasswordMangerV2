88df758f17dee212823e8d7da823527d680becf8
LaupWing <loc_strict@hotmail.com>
2023-03-13 12:13:59 +0100

    added cusotm tailwin

--- src/components/Elements/PasswordOptions.tsx
+++ src/components/Elements/PasswordOptions.tsx
@@ -28,7 +28,7 @@ export const PasswordOptions = () => {
                type="number" 
                className="p-1 w-12 bg-main-tertiare rounded text-white tracking-widest my-1 focus:outline-none focus:ring-2 focus:border-blue-600"
             />
-            <input type="checkbox" className="w-10" />
+            <input type="checkbox" className="w-5 h-5 text-white bg-blue-600" />
          </div>
       </div>
    )
