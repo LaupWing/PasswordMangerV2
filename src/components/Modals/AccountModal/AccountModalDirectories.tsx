47ebee8781c40ab2cd9626fc7fc2e7cac546161a
LaupWing <loc_strict@hotmail.com>
2023-03-15 10:52:53 +0100

    mapping

--- src/components/Modals/AccountModal/AccountModalDirectories.tsx
+++ src/components/Modals/AccountModal/AccountModalDirectories.tsx
@@ -17,21 +17,17 @@ export const AccountModalDirectories:FC<AccountModalDirectoriesProps> = ({
          <div className="border-2 relative mt-2 border-main-tertiare w-[345px] rounded p-3">
             <h2 className="uppercase text-xs text-main-tertiare font-bold tracking-wider absolute top-0 transform -translate-y-1/2 bg-main-primary px-1.5">Toegevoegd aan</h2>
             <div className="flex flex-col space-y-2">
-               <div className="flex items-center flex-1 justify-between">
-                  <span className="flex w-72 text-white/60 px-1">
-                     Test
-                  </span>
-                  <IconTrashcan 
-                     className="text-white cursor-pointer hover:text-red-500" 
-                     size={20} 
-                  />
-               </div>
-               <div className="flex items-center flex-1 justify-between">
-                  <span className="flex w-72 text-white/60 px-1">
-                     Test
-                  </span>
-                  <IconTrashcan className="text-white" size={20} />
-               </div>
+               {directories.map(directory => (
+                  <div className="flex items-center flex-1 justify-between">
+                     <span className="flex w-72 text-white/60 px-1">
+                        {directory}
+                     </span>
+                     <IconTrashcan 
+                        className="text-white cursor-pointer hover:text-red-500" 
+                        size={20} 
+                     />
+                  </div>
+               ))}
             </div>
          </div>
          <div className="flex flex-col space-y-4">
@@ -46,7 +42,6 @@ export const AccountModalDirectories:FC<AccountModalDirectoriesProps> = ({
       </div>
    )
 }
-
 
 const DirectoryDropdown = () => {
    const directories = [
