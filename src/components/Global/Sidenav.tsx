720e838b32d49200d49bad707af1ccc495dcb2ca
LaupWing <loc_strict@hotmail.com>
2023-03-15 11:21:32 +0100

    update

--- src/components/Global/Sidenav.tsx
+++ src/components/Global/Sidenav.tsx
@@ -2,9 +2,12 @@ import clsx from "clsx"
 import Link from "next/link"
 import { useRouter } from "next/router"
 import { FC, PropsWithChildren } from "react"
-import { IconStarFill, IconItems } from "~/components/Elements"
+import { IconStarFill, IconItems, IconDirectory } from "~/components/Elements"
+import { useAppSelector } from "~/redux/hooks"
 
 export const Sidenav = () => {
+   const { directories } = useAppSelector(state => state.accounts)
+   
    return (
       <div className="bg-main-secondary p-4 border-r-2 border-black">
          <img 
@@ -22,6 +25,16 @@ export const Sidenav = () => {
                Favoriete
             </SidenavLink>
             <li className="p-1 mt-2 text-white text-opacity-30 uppercase tracking-widest font-bold">Mappen</li>
+            {directories.map(directory => (
+               <SidenavLink 
+                  href={`directory/${directory.id}`}
+                  className="justify-between"
+               >
+                  {directory.name}
+
+                  <IconDirectory size={20}/>
+               </SidenavLink>
+            ))}
          </ul>
       </div>
    )
@@ -29,15 +42,17 @@ export const Sidenav = () => {
 
 interface SidenavLinkProps extends PropsWithChildren {
    href: string
+   className?: string
 }
 
-const SidenavLink:FC<SidenavLinkProps> = ({ children, href }) => {
+const SidenavLink:FC<SidenavLinkProps> = ({ children, href, className }) => {
    const router = useRouter()
    const baseStyles = "w-48 p-2 py-1 rounded-md flex items-center"
    const styles = clsx(
       baseStyles,
       router.pathname === href && "bg-blue-600",
-      href === "/" && router.asPath.includes("all") && "bg-blue-600"
+      href === "/" && router.asPath.includes("all") && "bg-blue-600",
+      className
    )
 
    return (
