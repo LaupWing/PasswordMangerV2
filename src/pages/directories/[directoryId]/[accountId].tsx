253e198569317f6977da53cc6a138dbcf78918e9
LaupWing <loc_strict@hotmail.com>
2023-03-16 13:34:10 +0100

    update

--- src/pages/directories/[directoryId]/[accountId].tsx
+++ src/pages/directories/[directoryId]/[accountId].tsx
@@ -2,7 +2,7 @@ import { NextPage } from "next"
 import Head from "next/head"
 import { useRouter } from "next/router"
 import { AccountsDetail } from "~/components/Global"
-import { fetchFavoriteAccounts, getDirectoryAccounts } from "~/slices/accountsSlice"
+import { getDirectoryAccounts } from "~/slices/accountsSlice"
 
 const DirectoryDetail:NextPage = () => {
    const router = useRouter()
