0d499b3208a17dcfb0579dfa16e83fdf91d7b0c0
LaupWing <loc_strict@hotmail.com>
2023-03-17 14:04:18 +0100

    udpate

--- src/slices/settings.ts
+++ src/slices/settings.ts
@@ -1,18 +1,32 @@
 import { createSlice, Dispatch, PayloadAction } from "@reduxjs/toolkit"
 
 interface SettingsState {
-   
+   xs: boolean
+   lg: boolean
+   md: boolean
 }
 
 const initialState:SettingsState = {
-   
+   xs: false,
+   lg: false,
+   md: false,
 }
 
 export const settingsSlice = createSlice({
    name: "settings",
    initialState,
    reducers: {
-      
+      watch_resize: (state) => {
+         state.lg = window.innerWidth < 1024
+         state.md = window.innerWidth < 678
+         state.xs = window.innerWidth < 420
+         
+         window.addEventListener("resize", () => {
+            state.lg = window.innerWidth < 1024
+            state.md = window.innerWidth < 678
+            state.xs = window.innerWidth < 420
+         })
+      }
    },
 })
 
