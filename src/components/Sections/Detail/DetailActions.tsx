b6960a198db0e17391694b685deeaf820cc3680f
LaupWing <loc_strict@hotmail.com>
2023-03-18 17:03:02 +0100

    udpate

--- src/components/Sections/Detail/DetailActions.tsx
+++ src/components/Sections/Detail/DetailActions.tsx
@@ -1,3 +1,4 @@
+import { useRouter } from "next/router"
 import { FC } from "react"
 import { IconEdit, IconTrashcan } from "~/components/Elements"
 
@@ -9,10 +10,19 @@ export const DetailActions:FC<DetailActionsProps> = ({
    showModal
 }) => {
    const action_btn_styles = "py-2 focus:outline-none focus:ring-2 focus:border-blue-600 px-4 bg-main-secondary rounded mr-4 flex items-center"
+   const router = useRouter()
+
+   const back = () => {
+      console.log(router.asPath.includes("directories"))
+      console.log(router)
+   }
 
    return (
       <div className="text-white text-sm flex justify-end">
-         <button className="bg-blue-600 uppercase text-xs tracking-wider font-bold flex justify-center items-center rounded mb-auto mr-auto py-1 px-4 md:hidden">
+         <button 
+            className="bg-blue-600 uppercase text-xs tracking-wider font-bold flex justify-center items-center rounded mb-auto mr-auto py-1 px-4 md:hidden"
+            onClick={back}
+         >
             Terug
          </button>
          <button 
