dfb46532a0b5e02f93221a294ed3ea524667ad46
LaupWing <loc_strict@hotmail.com>
2023-03-18 16:25:41 +0100

    update

--- src/components/Global/Layout.tsx
+++ src/components/Global/Layout.tsx
@@ -41,7 +41,7 @@ export const Layout:FC<PropsWithChildren> = ({children}) => {
             {show_sidenav && (
                <div 
                   className="absolute inset-0 z-50" 
-                  onClick={() => dispatch(toggleSidenav())}
+                  onClick={() => dispatch(toggleSidenav(false))}
                />
             )}
             <Topnav />
