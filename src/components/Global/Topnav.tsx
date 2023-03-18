7365fd8721d0509e5be0fdd32597d3eccf39e5fd
LaupWing <loc_strict@hotmail.com>
2023-03-18 16:07:13 +0100

    udpate

--- src/components/Global/Topnav.tsx
+++ src/components/Global/Topnav.tsx
@@ -4,7 +4,7 @@ import { auth } from "~/firebase"
 import { useAppDispatch, useAppSelector } from "~/redux/hooks"
 import { resetAccount } from "~/slices/accountsSlice"
 import { incrementTimer, logout, resetAuth, startTimer } from "~/slices/authSlice"
-import { IconChevron } from "../Elements"
+import { IconChevron, IconMenu } from "../Elements"
 import { Backdrop } from "./Backdrop"
 
 export const Topnav = () => {
@@ -36,8 +36,10 @@ export const Topnav = () => {
    },[])
    return (
       <div className="bg-main-secondary text-sm py-1 px-3 border-b-2 border-black text-main-tertiare hover:text-white flex items-center justify-between uppercase font-bold tracking-wider z-50 duration-150">
-         TopNav
-         <div className="flex space-x-4">
+         <div className="flex">
+            <IconMenu size={34} />
+         </div>
+         <div className="flex lg:space-x-4 space-x-1">
             <p className="mr-2 flex">
                Time left: <span className="w-11 text-center ml-1">{minutes}:{seconds}</span>
             </p>
