c043b9563ed4ccc10605229391b7c5b660ba0b93
LaupWing <loc_strict@hotmail.com>
2023-03-18 17:26:39 +0100

    update

--- src/components/Global/Backdrop.tsx
+++ src/components/Global/Backdrop.tsx
@@ -1,5 +1,6 @@
 import clsx from "clsx"
 import { FC, PropsWithChildren } from "react"
+import { useAppSelector } from "~/redux/hooks"
 
 interface BackdropProps extends PropsWithChildren {
    className?: string
@@ -11,13 +12,18 @@ export const Backdrop:FC<BackdropProps> = ({
    className = "",
    onClick = () => {}
 }) => {
+   const { show_sidenav, lg, nav_width } = useAppSelector(state => state.settings)
+   
    return (
       <div 
          className={clsx(
-            "flex fixed inset-0 z-50 bg-black/60",
+            "flex fixed left-0 top-0 w-screen h-screen z-50 bg-black/60",
             className
          )}
          onClick={onClick}
+         style={{
+            transform: `translateX(${(lg && !show_sidenav) ? nav_width : '0'}px)`
+         }}
       >
          { children }
       </div>
