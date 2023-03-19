7c2679c18993b322ff447575e843e99fd29574dd
LaupWing <loc_strict@hotmail.com>
2023-03-19 15:52:32 +0100

    update

--- src/components/Global/Topnav.tsx
+++ src/components/Global/Topnav.tsx
@@ -1,5 +1,5 @@
 import { useRouter } from "next/router"
-import { useEffect, useState } from "react"
+import { memo, useEffect, useState } from "react"
 import { auth } from "~/firebase"
 import { useAppDispatch, useAppSelector } from "~/redux/hooks"
 import { resetAccount } from "~/slices/accountsSlice"
@@ -8,41 +8,21 @@ import { toggleSidenav } from "~/slices/settings"
 import { IconChevron, IconMenu } from "../Elements"
 import { Backdrop } from "./Backdrop"
 
-export const Topnav = () => {
-   const { timer, expire_time, interval, start_time } = useAppSelector(state => state.auth)
+export const Topnav = memo(() => {
    const { accounts } = useAppSelector(state => state.accounts)
    const router = useRouter()
    const dispatch = useAppDispatch()
-   const current_time = Math.floor(start_time / 1000) + timer
-   const time_left = Math.floor(expire_time/ 1000) - current_time
-   const minutes = Math.floor(time_left /  60) 
-   const seconds = (time_left - (Math.floor(time_left /60) * 60)) < 10 
-      ? `0${time_left - (Math.floor(time_left /60) * 60)}`
-      : time_left - (Math.floor(time_left /60) * 60)
-   
-   if((expire_time / 1000) < current_time){
-      clearInterval(interval)
-      auth.signOut()
-      router.push("/login")
-   }
+
    const [show_tooltip, setShowTooltip] = useState(false)
    const route_name = () => {
       if(router.asPath.includes("all")){
-         // console.log(router.query)
-         // console.log(accounts.find(x => x.id! === router.query.accountId))
+         console.log(router.query)
+         console.log(accounts.find(x => x.id! === router.query.accountId))
       }
    }
+   route_name()
    // console.log(route_name())
 
-   useEffect(() => {
-      if(auth.currentUser){
-         dispatch(startTimer(setInterval(() => {
-            dispatch(incrementTimer())
-
-         }, 1000)))
-         
-      } 
-   },[])
    return (
       <div className="bg-main-secondary text-sm py-1 px-3 border-b-2 border-black text-main-tertiare hover:text-white flex items-center justify-between uppercase font-bold tracking-wider z-50 duration-150">
          <div className="flex">
@@ -53,9 +33,7 @@ export const Topnav = () => {
             />
          </div>
          <div className="flex lg:space-x-4 space-x-1">
-            <p className="mr-2 flex">
-               Time left: <span className="w-11 text-center ml-1">{minutes}:{seconds}</span>
-            </p>
+            <TimesLeft />
             <div className="relative">
                <h2 
                   className="flex space-x-1 items-center cursor-pointer"
@@ -74,6 +52,40 @@ export const Topnav = () => {
             />
          )}
       </div>
+   )
+})
+
+const TimesLeft = () => {
+   const dispatch = useAppDispatch()
+   const { timer, expire_time, interval, start_time } = useAppSelector(state => state.auth)
+   const current_time = Math.floor(start_time / 1000) + timer
+   const time_left = Math.floor(expire_time/ 1000) - current_time
+   const minutes = Math.floor(time_left /  60) 
+   const router = useRouter()
+   const seconds = (time_left - (Math.floor(time_left /60) * 60)) < 10 
+      ? `0${time_left - (Math.floor(time_left /60) * 60)}`
+      : time_left - (Math.floor(time_left /60) * 60)
+   
+   if((expire_time / 1000) < current_time){
+      clearInterval(interval)
+      auth.signOut()
+      router.push("/login")
+   }
+
+   useEffect(() => {
+      if(auth.currentUser){
+         dispatch(startTimer(setInterval(() => {
+            dispatch(incrementTimer())
+
+         }, 1000)))
+         
+      } 
+   },[])
+
+   return (
+      <p className="mr-2 flex">
+         Time left: <span className="w-11 text-center ml-1">{minutes}:{seconds}</span>
+      </p>
    )
 }
 
